729712ce600657cf7f4d21a0de478d5f2ad82661
Paz <mariapazibarra@MacBook-Air-de-Maria.local>
2026-03-27 15:26:12 -0300

    feat: display subscriptions using Angular signals and dynamic rendering

--- subscription-manager-ui/src/app/app.ts
+++ subscription-manager-ui/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SubscriptionService } from './services/subscription.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
